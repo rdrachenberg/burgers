9aed62abc93813f0394563acc5d66bb0ab5e7108
Ryan Drachenberg <ryan@Ryans-MacBook-Pro.local>
2018-03-01 00:41:46 -0500

    added remost jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,19 @@
 var mysql = require("mysql");
-var jaws = 'mysql://g9ie8iu61hpcw6py:rvtpykrnly9u08wv@nj5rh9gto1v5n05t.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/vsrh7h88use4zgvq';
 
-
-if(process.env.JAWDB_URL) {
-    connection = mysql.createConnection(process.env.jaws);
-} else {
+if (port === 3008) {
     var connection = mysql.createConnection({
         host: "localhost",
         user: "root",
         password: "",
         database: "burgers_db"
     });
-};
-
+} else {
+    var connection = mysql.createConnection({
+        host: 'nj5rh9gto1v5n05t.cbetxkdyhwsb.us - east - 1.rds.amazonaws.com',
+        user: 'g9ie8iu61hpcw6py',
+        password: 'rvtpykrnly9u08wv'
+    })
+}
 connection.connect(function (err) {
     if (err) {
         console.error("error connecting: " + err.stack);
