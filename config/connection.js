2532e53164dfed4f96604dc533c1778e34f242f7
Ryan Drachenberg <ryan@Ryans-MacBook-Pro.local>
2018-03-01 01:34:41 -0500

    edited out amazon remote connect

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,19 @@
 var mysql = require("mysql");
 
-if (port === 3008) {
+
     var connection = mysql.createConnection({
         host: "localhost",
         user: "root",
         password: "",
-        database: "burgers_db"
+        database: "burgers_db",
+        port: "3008"
     });
-} else {
-    var connection = mysql.createConnection({
-        host: 'nj5rh9gto1v5n05t.cbetxkdyhwsb.us - east - 1.rds.amazonaws.com',
-        user: 'g9ie8iu61hpcw6py',
-        password: 'rvtpykrnly9u08wv'
-    })
-}
+    // var connection = mysql.createConnection({
+    //     host: 'nj5rh9gto1v5n05t.cbetxkdyhwsb.us - east - 1.rds.amazonaws.com',
+    //     user: 'g9ie8iu61hpcw6py',
+    //     password: 'rvtpykrnly9u08wv'
+    // });
+
 connection.connect(function (err) {
     if (err) {
         console.error("error connecting: " + err.stack);
