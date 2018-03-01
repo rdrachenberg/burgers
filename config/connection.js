16489f838d5a7a6261206213ed2bf8f13cc29017
Ryan Drachenberg <ryan@Ryans-MacBook-Pro.local>
2018-03-01 02:43:16 -0500

    clean up

--- config/connection.js
+++ config/connection.js
@@ -11,12 +11,6 @@ if (process.env.JAWSDB_URL) {
             port: "3008",
         });
     };
-    // var connection = mysql.createConnection({
-    //     host: 'nj5rh9gto1v5n05t.cbetxkdyhwsb.us - east - 1.rds.amazonaws.com',
-    //     user: 'g9ie8iu61hpcw6py',
-    //     password: 'rvtpykrnly9u08wv'
-    // });
-
 connection.connect(function (err) {
     if (err) {
         console.error("error connecting: " + err.stack);
