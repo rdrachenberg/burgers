b108aa9f02754cabdb047035368101322246806d
Ryan Drachenberg <ryan@Ryans-MacBook-Pro.local>
2018-02-28 22:29:15 -0500

    final push working app

--- server.js
+++ server.js
@@ -34,5 +34,5 @@ var routes = require("./controllers/burgers_controller.js");
 app.use("/", routes);
 
 app.listen(PORT, function () {
-    console.log("App now listening at localhost:" + PORT);
+    console.log("App now listening at localhost:  " + PORT);
 });
